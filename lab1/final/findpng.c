e7b9c3a46169fb2925f4866633a28156964ee314
Young Ha Ahn <jason91214@gmail.com>
2021-09-25 22:36:01 -0400

    hi

--- lab1/final/findpng.c
+++ lab1/final/findpng.c
@@ -77,6 +77,7 @@ void findpng(DIR *folder, int *pngExists, char path[]) {
         } else if(entry->d_type == DT_REG) {
             printf("7\n");
             FILE *file = fopen(entry->d_name, "rb");
+            printf("8\n");
             if(ispng(file)) {
                 printf("%s/%s", path, entry->d_name);
                 *pngExists = 1;
