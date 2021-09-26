11dd9b23b0bb57e40063205310739971a00d62f9
Young Ha Ahn <jason91214@gmail.com>
2021-09-25 22:11:16 -0400

    hi

--- lab1/final/findpng.c
+++ lab1/final/findpng.c
@@ -55,10 +55,10 @@ void findpng(DIR *folder, int *pngExists, char path[]) {
         if(entry->d_type == DT_DIR && strcmp(entry->d_name,".") != 0 && strcmp(entry->d_name,"..") != 0) {
             int j = 0;
             for(int i = 0; j < 1+strlen(entry->d_name); i++) {
-                if(strcmp(path[i],"") && j == 0) {
-                    path[i] = "/";
+                if(path[i] == '' && j == 0) {
+                    path[i] = '/';
                     j++;
-                } else if(strcmp(path[i],"")) {
+                } else if(path[i] == '') {
                     path[i] == entry->d_name[j-1];
                     j++;
                 }
