dc3893e607de76cb47d3e89695c1fe9ea3049d20
Young Ha Ahn <jason91214@gmail.com>
2021-09-25 20:06:41 -0400

    pnginfo

--- lab1/final/pnginfo.c
+++ lab1/final/pnginfo.c
@@ -10,8 +10,10 @@ int main(int argc, char **argv);
 int ispng(FILE *f);
 
 int main(int argc, char **argv){
-    U8 *f_IHDR_crc_input = malloc(17);
-    U8 *f_IHDR_crc = malloc(sizeof(U8) *4);
+    U8 *f_crc_input = malloc(17);
+    U8 *f_length = malloc(sizeof(U8)*4);
+    U32 length = 0;
+    U8 *f_crc = malloc(sizeof(U8) *4);
     U8 *f_png_width = malloc(sizeof(U8) *4);
     U32 png_width = 0;
     U8 *f_png_height = malloc(sizeof(U8) *4);
@@ -30,24 +32,62 @@ int main(int argc, char **argv){
         
         fseek(png, 12, SEEK_SET);
 
-        fread(f_IHDR_crc_input, 1, 17, png);
-        fread(f_IHDR_crc, 1, 4, png);
+        fread(f_crc_input, 1, 17, png);
+        fread(f_crc, 1, 4, png);
 
-        crc_calc = crc(f_IHDR_crc_input, 17);
+        crc_calc = crc(f_crc_input, 17);
         for(int i = 0; i < 4; i++) {
-            if(((crc_calc >> (8*i)) & 0xff) != f_IHDR_crc[i]) {
+            if(((crc_calc >> (8*i)) & 0xff) != f_crc[i]) {
                 crc_match = 0;
             }
         }
         
+        if(crc_match == 0) {
+            printf("%s: %u x $u\n", argv[1], *f_png_width, *f_png_height);
+            printf("IDHL chunk CRC error: computed %x, expected %x\n", *crc_calc, *f_crc);
+        } else {
+            fread(f_length,1,4,png)
+            memcpy(&length, f_length, sizeof(length));
 
-        //*(&crc_calc + 0x2)
-        
-        
+            realloc(f_crc_input, 4+length);
+            fread(f_crc_input, 4+length, 1, png);
+            fread(f_crc, 4, 1, png);
+
+            crc_calc = crc(f_crc_input, 4+length);
+            for(int i = 0; i < 4; i++) {
+                if(((crc_calc >> (8*i)) & 0xff) != f_crc[i]) {
+                    crc_match = 0;
+                }
+            }
+            
+            if(crc_match == 0) {
+                printf("%s: %u x $u\n", argv[1], *f_png_width, *f_png_height);
+                printf("IDAT chunk CRC error: computed %x, expected %x\n", *crc_calc, *f_crc);
+            } else {
+                fseek(png, 4, SEEK_CUR);
+                fread(f_crc_input, 4, 1, png);
+                fread(f_crc, 4, 1, png);
+
+                crc_calc = crc(f_crc_input, 4);
+                for(int i = 0; i < 4; i++) {
+                    if(((crc_calc >> (8*i)) & 0xff) != f_crc[i]) {
+                    crc_match = 0;
+                    }
+                }
+                
+                if(crc_match == 0) {
+                    printf("%s: %u x $u\n", argv[1], *f_png_width, *f_png_height);
+                    printf("IEND chunk CRC error: computed %x, expected %x\n", *crc_calc, *f_crc);
+                } else {
+                    printf("%s: %u x $u\n", argv[1], f_png_width, f_png_height);
+                }
+            }
+        }
     }
     
-    free(f_IHDR_crc_input);
-    free(f_IHDR_crc);
+    free(f_crc_input);
+    free(f_length);
+    free(f_crc);
     free(f_png_width);
     free(f_png_height);
     fclose(png);
