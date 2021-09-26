4de2738ab520a92039ece7df86218d424fd04d9a
Young Ha Ahn <jason91214@gmail.com>
2021-09-26 17:13:19 -0400

    first

---
+++ lab1/final/catpng.c
@@ -0,0 +1,50 @@
+#include <stdio.h>    /* for printf(), perror()...   */
+#include <stdlib.h>   /* for malloc()                */
+#include <errno.h>    /* for errno                   */
+#include "crc.h"      /* for crc()                   */
+#include "zutil.h"    /* for mem_def() and mem_inf() */
+#include "lab_png.h"  /* simple PNG data structures  */
+#include <arpa/inet.h>
+#include <dirent.h>
+
+
+int main(int argc, char **argv);
+int ispng(FILE *f);
+
+int main(int argc, char **argv) {
+    int stop = 0;
+    FILE *files[argc-1] = malloc(sizeof(FILE)*(argc-1));
+    int *order[argc-1] = malloc(sizeof(int)*(argc-1));
+    for(int i = 1; i < argc; i++) {
+        for(int j = 1; j < argc; j++) {
+            if(argc[j][strlen(argc[j])-5] == (char)i) {
+                files[i-1] = fopen(argv[j], "rb");
+                order[i-1] = j;
+            }
+        }
+    }
+
+    for(int s = 0; s < argc-1; s++) {
+        printf("%s\n", argv[order[s]]);
+    }
+
+
+    return 0;
+}
+
+
+int ispng(FILE *f){
+    U8 *header = malloc(sizeof(U8)*8);
+    int pngCode[8] = {137, 80, 78, 71, 13, 10, 26, 10};
+    rewind(f);
+    fread(header, 8, 1, f);
+    rewind(f);
+    for(int i = 0; i < 8; i++) {
+        if(header[i] != pngCode[i]) {
+            free(header);
+            return 0;
+        }
+    }
+    free(header);
+    return 1;
+}
