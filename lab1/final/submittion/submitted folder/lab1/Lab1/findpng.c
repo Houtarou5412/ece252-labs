8e02606deda7c195d3b385f0bf77229a9baeae9b
Young Ha Ahn <jason91214@gmail.com>
2021-10-08 05:30:08 -0400

    testing

---
+++ lab1/final/submittion/submitted folder/lab1/Lab1/findpng.c
@@ -0,0 +1,114 @@
+#include <stdio.h>    /* for printf(), perror()...   */
+#include <stdlib.h>   /* for malloc()                */
+#include <errno.h>    /* for errno                   */
+#include "crc.h"      /* for crc()                   */
+#include "zutil.h"    /* for mem_def() and mem_inf() */
+#include "lab_png.h"  /* simple PNG data structures  */
+#include <arpa/inet.h>
+#include <dirent.h>
+
+
+int main(int argc, char **argv);
+void findpng(DIR *folder, int *pngExists, char path[]);
+int ispng(FILE *f);
+
+int main(int argc, char **argv) {
+    //printf("arg: %s\n", argv[1]);
+    DIR *folder = NULL;
+    int *pngExists = malloc(sizeof(int));
+    *pngExists = 0;
+    char path[1000] = {'\0'};
+    for(int i = 0; i < strlen(argv[1]); i++) {
+        path[i] = argv[1][i];
+    }
+    folder = opendir(argv[1]);
+    //printf("main path: %s\n", path);
+    if(folder != NULL) {
+        //printf("2\n");
+        findpng(folder, pngExists, path);
+        closedir(folder);
+    }
+
+    if(*pngExists == 0) {
+        printf("findpng: No PNG file found\n");
+    }
+    free(pngExists);
+    return 0;
+}
+
+int ispng(FILE *f){
+    U8 *header = malloc(sizeof(U8)*8);
+    int pngCode[8] = {137, 80, 78, 71, 13, 10, 26, 10};
+    rewind(f);
+    fread(header, 8, 1, f);
+    rewind(f);
+    for(int i = 0; i < 8; i++) {
+        if(header[i] != pngCode[i]) {
+            free(header);
+            return 0;
+        }
+    }
+    free(header);
+    return 1;
+}
+
+void findpng(DIR *folder, int *pngExists, char path[]) {
+    struct dirent *entry;
+    //printf("3\n");
+    while(1) {
+        entry = readdir(folder);
+        if(entry == NULL || strcmp(entry->d_name, "\0") == 0) {
+            break;
+        }
+        //printf("4\n");
+        if(entry->d_type == DT_DIR && strcmp(entry->d_name,".") != 0 && strcmp(entry->d_name,"..") != 0 && strcmp(entry->d_name, "\0") != 0) {
+            //printf("dir name: %s\n", entry->d_name);
+            char filepath[1000] = {'\0'};
+            int j = 0;
+            for(int i = 0; j < 1+strlen(entry->d_name); i++) {
+                //printf("8\n");
+                if(path[i] != '\0') {
+                    filepath[i] = path[i];
+                } else if(j == 0) {
+                    filepath[i] = '/';
+                    j++;
+                } else {
+                    filepath[i] = entry->d_name[j-1];
+                    j++;
+                }
+            }
+            //printf("dir path: %s\n", path);
+            DIR *subfolder = opendir(filepath);
+            findpng(subfolder, pngExists, filepath);
+            closedir(subfolder);
+        } else if(entry->d_type == DT_REG) {
+            //printf("7\n");
+            char filepath[1000] = {'\0'};
+            int j = 0;
+            for(int i = 0; j < 1+strlen(entry->d_name); i++) {
+                //printf("8\n");
+                if(path[i] != '\0') {
+                    filepath[i] = path[i];
+                } else if(j == 0) {
+                    filepath[i] = '/';
+                    j++;
+                } else {
+                    filepath[i] = entry->d_name[j-1];
+                    j++;
+                }
+            }
+            //printf("path is %s\n", filepath);
+            FILE *file = fopen(filepath, "rb");
+            //printf("entry name: %s\n", entry->d_name);
+            if(file != NULL && ispng(file)) {
+                printf("%s\n", filepath);
+                *pngExists = 1;
+            }
+            //printf("9\n");
+            if(file != NULL) {
+                fclose(file);
+            }
+        }
+    }
+    entry = NULL;
+}
---
+++ lab1/final/submittion/submitted folder/lab1/Lab1/pnginfo.c
@@ -0,0 +1,126 @@
+#include <stdio.h>    /* for printf(), perror()...   */
+#include <stdlib.h>   /* for malloc()                */
+#include <errno.h>    /* for errno                   */
+#include "crc.h"      /* for crc()                   */
+#include "zutil.h"    /* for mem_def() and mem_inf() */
+#include "lab_png.h"  /* simple PNG data structures  */
+#include <arpa/inet.h>
+
+int main(int argc, char **argv);
+int ispng(FILE *f);
+
+int main(int argc, char **argv){
+    U8 *filename = malloc(sizeof(U8)*200);
+    int k = 0;
+    for(int i = 0; argv[1][i] != '\0'; i++) {
+        if(argv[1][i] == '/') {
+            for(int j = 0; j < i; j++) {
+                filename[j] = '\0';
+                k = 0;
+            }
+        } else {
+            filename[k] = argv[1][i];
+            k++;
+        }
+        //printf("%s out of %d\n", filename, sizeof(argv[1]));
+    }
+    U8 *f_crc_input = malloc(17);
+    U8 *f_length = malloc(sizeof(U8)*4);
+    U32 length = 0;
+    U8 *f_crc = malloc(sizeof(U8) *4);
+    U32 crc_val = 0;
+    U8 *f_png_width = malloc(sizeof(U8) *4);
+    U32 png_width = 0;
+    U8 *f_png_height = malloc(sizeof(U8) *4);
+    U32 png_height = 0;
+    U32 crc_calc = 0;
+    FILE *png;
+    png = fopen(argv[1], "rb");
+    if(png == NULL || ispng(png) == 0){
+        printf("%s: Not a PNG file\n",filename);
+    } else {
+        fseek(png, 16, SEEK_CUR);
+        fread(f_png_width, 1, 4, png);
+        fread(f_png_height, 1, 4, png);
+
+        memcpy(&png_width, f_png_width, sizeof(png_width));
+        memcpy(&png_height, f_png_height, sizeof(png_height));
+        png_width = (U32)ntohl(png_width);
+        png_height = (U32)ntohl(png_height);
+        
+        
+        fseek(png, 12, SEEK_SET);
+
+        fread(f_crc_input, 1, 17, png);
+        fread(f_crc, 1, 4, png);
+        memcpy(&crc_val, f_crc, sizeof(crc_val));
+        crc_val = (U32)ntohl(crc_val);
+
+        crc_calc = crc(f_crc_input, 17);
+        
+        if(crc_calc != crc_val) {
+            printf("%s: %u x %u\n", filename, png_width, png_height);
+            printf("IDHL chunk CRC error: computed %x, expected %x\n", crc_calc, crc_val);
+        } else {
+            fread(f_length,1,4,png);
+            memcpy(&length, f_length, sizeof(length));
+            length = (U32)ntohl(length);
+
+            f_crc_input = realloc(f_crc_input, 4+length);
+            fread(f_crc_input, 4+length, 1, png);
+            fread(f_crc, 4, 1, png);
+            memcpy(&crc_val, f_crc, sizeof(crc_val));
+            crc_val = (U32)ntohl(crc_val);
+
+            crc_calc = crc(f_crc_input, 4+length);
+
+            if(crc_calc != crc_val) {
+                printf("%s: %u x %u\n", filename, png_width, png_height);
+                printf("IDAT chunk CRC error: computed %x, expected %x\n", crc_calc, crc_val);
+            } else {
+                fseek(png, 4, SEEK_CUR);
+                fread(f_crc_input, 4, 1, png);
+                fread(f_crc, 4, 1, png);
+                memcpy(&crc_val, f_crc, sizeof(crc_val));
+                crc_val = (U32)ntohl(crc_val);
+
+                crc_calc = crc(f_crc_input, 4);
+                
+                if(crc_calc != crc_val) {
+                    printf("%s: %u x %u\n", filename, png_width, png_height);
+                    printf("IEND chunk CRC error: computed %x, expected %x\n", crc_calc, crc_val);
+                } else {
+                    printf("%s: %u x %u\n", filename, png_width, png_height);
+                }
+            }
+        }
+    }
+    
+    free(f_crc_input);
+    free(f_length);
+    free(f_crc);
+    free(f_png_width);
+    free(f_png_height);
+    free(filename);
+    if(png != NULL) {
+        fclose(png);
+    }
+    return 0;
+}
+
+int ispng(FILE *f){
+    U8 *header = malloc(sizeof(U8)*8);
+    int pngCode[8] = {137, 80, 78, 71, 13, 10, 26, 10};
+    rewind(f);
+    fread(header, 8, 1, f);
+    rewind(f);
+    for(int i = 0; i < 8; i++) {
+        if(header[i] != pngCode[i]) {
+            free(header);
+            return 0;
+        }
+    }
+    free(header);
+    return 1;
+}
+
