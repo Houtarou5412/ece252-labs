b57028b201e403f067cf36082fd6a466125bb7b0
Young Ha Ahn <jason91214@gmail.com>
2021-10-08 17:21:03 -0400

    testing

--- lab2/final/main_write_header_cb.c
+++ lab2/final/main_write_header_cb.c
@@ -43,7 +43,7 @@
      _a > _b ? _a : _b; })
 
 typedef struct recv_buf2 {
-    char *buf;       /* memory to hold a copy of received data */
+    U8 *buf;       /* memory to hold a copy of received data */
     size_t size;     /* size of valid data in buf in bytes*/
     size_t max_size; /* max capacity of buf in bytes*/
     int seq;         /* >=0 sequence number extracted from http header */
