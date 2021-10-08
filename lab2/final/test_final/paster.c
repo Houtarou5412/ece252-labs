7e71f50df1716368334fc766897dc7d2a0f1f549
Young Ha Ahn <jason91214@gmail.com>
2021-10-08 18:07:38 -0400

    testing

--- lab2/final/test_final/paster.c
+++ lab2/final/test_final/paster.c
@@ -12,7 +12,7 @@
 #include <pthread.h>
 
 #define IMG_URL "http://ece252-1.uwaterloo.ca:2520/image?img=1"
-#define DUM_URL "https://example.com/"
+#define ECE252_HEADER "X-Ece252-Fragment: "
 #define BUF_SIZE 1048576  /* 1024*1024 = 1M */
 #define BUF_INC  524288   /* 1024*512  = 0.5M */
 
@@ -32,7 +32,7 @@ typedef struct recv_buf2 {
 //FUNCTION DECLARATIONS
 int main(int argc, char **argv);
 int ispng(FILE *f);
-int catpng(int argc, RECV_BUF recvbuf[]);
+int catpng(int argc, RECV_BUF * recvbuf);
 size_t header_cb_curl(char *p_recv, size_t size, size_t nmemb, void *userdata);
 size_t write_cb_curl3(char *p_recv, size_t size, size_t nmemb, void *p_userdata);
 int recv_buf_init(RECV_BUF *ptr, size_t max_size);
