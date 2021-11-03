e3f101fb194a054a73f64f1b28ed6e31ef28561e
Young Ha Ahn <jason91214@gmail.com>
2021-11-03 18:58:35 -0400

    test

--- lab3/final/paster2.c
+++ lab3/final/paster2.c
@@ -247,6 +247,7 @@ int main(int argc, char **argv) {
                 pthread_mutex_unlock(mutex);
                 break;
             }
+            pthread_mutex_unlock(mutex);
             g++;
         }
 
