b5422ea9253bbb8c26a8c0f7941dda633e83ff33
Young Ha Ahn <jason91214@gmail.com>
2021-11-03 17:21:23 -0400

    test

--- lab3/final/paster2.c
+++ lab3/final/paster2.c
@@ -74,7 +74,7 @@ int main(int argc, char **argv) {
             perror("shmget");
             abort();
         }
-        printf("test1\n");
+        printf("buffer_size: %d\n", buffer_size);
 
         p_shm_recv_buf[t] = shmat(shmid[t], NULL, 0);
         printf("got %dth shm\n", t);
